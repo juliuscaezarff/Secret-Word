4e16d8bdba3ed20a51d0ab2dd78ce79b5bec93ec
Julius Caezar <julius.caezar25@gmail.com>
2022-09-02 10:32:26 -0400

    add modifications

---
+++ src/components/StartScreen.jsx
@@ -0,0 +1,12 @@
+const StartScreen = () => {
+
+  return (
+    <div>
+      <h1>Secret Word</h1>
+      <p>Clique no botão abaixo para começar a jogar!</p>
+      <button>Começar o jogo</button>
+    </div>
+  )
+}
+
+export default StartScreen
