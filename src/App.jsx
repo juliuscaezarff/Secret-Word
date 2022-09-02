988f88d9e748cbb318cfbb2f4bd9b5a0a963d37e
Julius Caezar <julius.caezar25@gmail.com>
2022-09-02 11:05:23 -0400

    add hooks and datas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
+// CSS
 import './App.css'
 
+// React
+import { useCallback, useEffect, useState } from 'react'
+
+// Data
+import { wordsList } from './data/Word'
+
+// Components
 import StartScreen from './components/StartScreen'
 
+
+const stages = [
+  {id: 1, name: "start"},
+  {id: 2, name: "game"},
+  {id: 3, name: "end"}
+]
+
 function App() {
+
+  const [gameStage, setGameStage] = useState(stages[0].name)
   
   return (
     <div className="App">
-      <StartScreen />
+      {gameStage === "start" && <StartScreen />}
+      {gameStage === "game" && <StartScreen />}
+      {gameStage === "end" && <StartScreen />}
+
     </div>
   )
 }
